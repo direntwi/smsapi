77dee4c05d0eb82995469dc6d849e5a92fa3f66b
Yoocee Ansah <yooceeansah@gmail.com>
2024-10-18 18:37:13 +0100

    created sms app

---
+++ sms/views.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+# from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+# @csrf_exempt
+def send_sms(request):
+    if request.method == 'POST':
+        # Handle SMS sending logic
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        data = {
+            "key" : settings.KEY,
+            "msisdn" : phone,
+            "message" :  message,
+            "sender_id" : settings.SENDER_ID,
+        }
+
+        try:
+            response = requests.post(settings.API_URL, data=data)
+            if response.status_code == 200:
+                return JsonResponse({"response": response.json()})
+            else:
+                return JsonResponse({'success': False, 'message': 'Failed to send message.'})
+        except Exception as e:
+            return JsonResponse({'success': False, 'message': f'Error: {str(e)}'})
+
+    return render(request, 'sms/send_sms.html')
